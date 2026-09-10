2d6537ad71e26436181d0a9110444afd31b0dc3c
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-10 17:30:59 +0530

    this is update the code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/code.java
@@ -0,0 +1,13 @@
+public class code {
+    public static void main(String[] args) {
+        int javaHoursPerDay = 2;
+        int aptitudeHoursPerDay = 1;
+        int numberOfDays = 5;
+        int weeklyJavaHours = javaHoursPerDay * numberOfDays;
+        int weeklyAptitudeHours = aptitudeHoursPerDay * numberOfDays;
+        int totalPreparationHours = weeklyJavaHours + weeklyAptitudeHours;
+        System.out.println("java: " + weeklyJavaHours);
+        System.out.println("java: " + weeklyAptitudeHours);
+        System.out.println("java: " + totalPreparationHours);
+    }
+    }    
