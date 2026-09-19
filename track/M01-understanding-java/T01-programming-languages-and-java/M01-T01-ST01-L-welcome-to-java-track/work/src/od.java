896ad4fe72ff5d853040743f83b80a7022026fcb
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-19 10:04:40 +0530

    updated this code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/od.java
@@ -0,0 +1,10 @@
+public class od {
+    public static void main (String[] args) {
+        int number = 14;
+        if (number > 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+    }
+}
