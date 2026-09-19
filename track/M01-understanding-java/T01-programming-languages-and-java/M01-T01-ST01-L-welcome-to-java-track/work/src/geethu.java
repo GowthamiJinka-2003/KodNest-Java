9e0e0ae9c7ec92865a450de7ad3c660826608692
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-19 10:00:16 +0530

    updated this code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/geethu.java
@@ -0,0 +1,12 @@
+public class geethu {
+    public static void main (String[] args) {
+        int number = -8;
+        if (number > 0) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
