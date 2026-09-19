feabc666b400a054aa5abb92ddb6af6c7bfaa6a0
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-19 09:52:56 +0530

    updated this code

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/mark.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/mark.java
@@ -6,7 +6,7 @@ public class mark {
         if (marks >= 60 && attendance >= 75) {
             System.out.println("Placement Ready");
         for (int i = 1; i <= 3; i++) {
-            if (i == 3) {
+            if (i == 4) {
                 break;
             }
             System.out.println("Practice Day: " + i);
