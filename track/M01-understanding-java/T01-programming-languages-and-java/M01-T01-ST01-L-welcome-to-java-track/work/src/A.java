87aed6a790832369eab9ae6cc88d1689f1cad034
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-20 15:50:26 +0530

    updated this code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/A.java
@@ -0,0 +1,15 @@
+public class A {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 3)
+        {
+            int j = 1;
+            while (j <= 2)
+            {
+                System.out.println(i + "," + j);
+                j++;
+            }
+            i++;
+        }
+    }
+}
