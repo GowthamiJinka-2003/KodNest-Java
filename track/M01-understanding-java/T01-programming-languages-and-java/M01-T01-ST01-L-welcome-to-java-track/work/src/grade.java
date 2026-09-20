a55b3ceb9fb75b88b5430d662424116749ddbd0c
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-20 15:22:36 +0530

    updated this code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/grade.java
@@ -0,0 +1,15 @@
+public class grade {
+    public static void main(String[] args) {
+        int marks = 76;
+        if (marks >= 80) {
+            System.out.println("Grade: A");
+        } else if (marks > 60 && marks < 79) {
+            System.out.println("Grade: B");
+        } else if (marks < 40 && marks > 59) {
+            System.out.println("Grade: C");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+    
+}
