fdd65f563ddecbe202540009333fea53a120cd35
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-20 15:36:11 +0530

    updated this code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/nu.java
@@ -0,0 +1,8 @@
+public class nu {
+    public static void main(String[] args) {
+        int number = 5;
+        for (int i = 1; i <= 5; i++) {
+            System.out.println(number + " X " + i + " = " + (number * i));
+        }
+    }
+}
