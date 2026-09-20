ae0956c6d74ef69cbc34dd3c375cff98cd1de266
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-20 15:11:37 +0530

    updated this code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/ab.java
@@ -0,0 +1,13 @@
+public class ab {
+    public static void main(String[] args) {
+        int first = 25;
+        int second = 40;
+        if (first < second) {
+            System.out.println("Larger: " + second);
+        } else if (first > second) {
+            System.out.println("Larger: "  + first);
+        } else {
+            System.out.println("Equal");
+        }
+    }
+}
