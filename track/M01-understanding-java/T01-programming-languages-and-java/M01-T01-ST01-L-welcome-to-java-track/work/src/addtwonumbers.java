337a76ea7a4e2fa35a68920be37666c32f1fac09
GowthamiJinka-2003 <gowthamijinka2003@gmail.com>
2026-09-26 10:47:54 +0530

    updated this code

---
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/addtwonumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class addtwonumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Sum: " + sum);
+
+        scanner.close();
+    }
+}
